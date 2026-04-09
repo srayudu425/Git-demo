a488427948bd5ce9d46690b00631300dde891f86
Sanjeev <rsanjeeva2414@gmail.com>
2026-04-09 13:12:20 +0000

    appservice

--- storage/main.tf
+++ storage/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "tfstate_rg" {
 
 # Storage Account
 resource "azurerm_storage_account" "tfstate_sa" {
-  name                     = "tfstate12345" # must be globally unique
+  name                     = "tfstatestapp123" # must be globally unique
   resource_group_name      = azurerm_resource_group.tfstate_rg.name
   location                 = azurerm_resource_group.tfstate_rg.location
   account_tier             = "Standard"
